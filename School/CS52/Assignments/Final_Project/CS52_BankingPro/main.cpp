d1462863ca25788eef67d1713a7b4de2b560f782
fletcher <fletcher@clamshell8>
2017-04-26 17:08:27 -0700

    This is a test commit of an empty main.cpp file.

---
+++ School/CS52/Assignments/Final_Project/CS52_BankingPro/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Fletcher Kauffman
+ * kauffman_fletcher_el@student.smc.edu
+ * 
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: fletcher
+ *
+ * Created on April 26, 2017, 5:06 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
